7ecf6fdbaafcccdce47cf0226037ff4ccc390298
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-10-05 13:15:31 +0100

    Controllable Turtle with cars spawning & moving

--- main.py
+++ main.py
@@ -8,7 +8,22 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(fun=player.go_up,key="Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car() #Roughly every 6 times the while loop runs, there will be a new car
+    car_manager.move_cars()
+
+
+screen.exitonclick()
+
+
+
