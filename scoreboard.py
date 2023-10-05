9bd199a19a3e44175a864b36f0c277bc40551bb4
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-10-05 15:38:24 +0100

    Class to manage the score, and write messages

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,22 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.score = 0
+        self.update_scoreboard()
+    
+    def update_scoreboard(self):
+        self.clear()
+
+        self.goto(x=-235, y=265)
+        self.write(f"Level:{self.score}", align="center", font=(FONT))
+        self.score += 1
+    
+    def game_over(self):
+        self.goto(x=0, y=0)
+        self.write("GAME OVER...", align="center", font=(FONT))
